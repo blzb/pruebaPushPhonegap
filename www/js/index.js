336412ee4b1dc335e522d3afc8ef1506430af5fb
Angel pimentel <angelpmza@gmail.com>
2013-02-21 19:10:03 -0600

    add register for push

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+function storeToken(token)
+{
+    var xmlhttp = new XMLHttpRequest();
+    console.log("Token to register " + token);
+    xmlhttp.open("GET", "http://192.168.1.186:8180/gcm-demo/register", true);
+    xmlhttp.send("regId="+token+"&message=pushnotificationtester");
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4) {
+            //a response now exists in the responseTest property.
+            console.log("Registration response: " + xmlhttp.responseText);
+        }
+    }
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -44,17 +58,26 @@ var app = {
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
-        alert('Success! Result = '+result)
-    },
+        storeToken(result);
+        alert('Success! Result = '+result)        
+    },    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var pushNotification = window.plugins.pushNotification;
         // TODO: Enter your own GCM Sender ID in the register call for Android
         if (device.platform == 'android' || device.platform == 'Android') {
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"157854791681","ecb":"app.onNotificationGCM"});
+            pushNotification.register(this.successHandler, this.errorHandler,{
+                "senderID":"157854791681",
+                "ecb":"app.onNotificationGCM"
+            });
         }
         else {
-            pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+            pushNotification.register(this.tokenHandler,this.errorHandler,{
+                "badge":"true",
+                "sound":"true",
+                "alert":"true",
+                "ecb":"app.onNotificationAPN"
+            });
         }
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -95,21 +118,21 @@ var app = {
                     // here is where you might want to send it the regID for later use.
                     alert('registration id = '+e.regid);
                 }
-            break;
+                break;
 
             case 'message':
-              // this is the actual push notification. its format depends on the data model
-              // of the intermediary push server which must also be reflected in GCMIntentService.java
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
+                // this is the actual push notification. its format depends on the data model
+                // of the intermediary push server which must also be reflected in GCMIntentService.java
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
 
             case 'error':
-              alert('GCM error = '+e.msg);
-            break;
+                alert('GCM error = '+e.msg);
+                break;
 
             default:
-              alert('An unknown GCM event has occurred');
-              break;
+                alert('An unknown GCM event has occurred');
+                break;
         }
     }
 
